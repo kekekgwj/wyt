604ef7eefc7dbb1494ec6073e5cf4f0e9c3ec400
weijieGuo <guo.weijie@outlook.com>
2021-02-18 05:30:03 +0800

    Add header

--- medical-device/src/pages/App.js
+++ medical-device/src/pages/App.js
@@ -9,9 +9,14 @@ import '../styles/App.css'
 function App() {
     return (
         <div className="content-container">
+            <Row className="header">
+                <span/>
+            </Row>
+            <Row className="content">
                 <div className="left-page"><LeftPage/></div>
                 <div className="main-page"><MainPage/></div>
                 <div className="right-page"><RightPage/></div>
+            </Row>
         </div>
     );
 }
