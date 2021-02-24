f452c0091cdbda45bfc6d895e2cad8ab31435e79
weijieGuo <guo.weijie@outlook.com>
2021-02-24 23:08:20 +0800

    Delete unused code

--- medical-device/src/pages/RightPage.jsx
+++ medical-device/src/pages/RightPage.jsx
@@ -1,76 +1,22 @@
-import React, { useState, useEffect, useReducer }from "react";
+import React, { useState, useEffect }from "react";
 
 import '../styles/rightPage.css';
-import { getDeviceNumbByLocation, getReportListData } from '../backend/api';
+import { getDeviceNumbByLocation } from '../backend/api';
 import  {pinyinConverter} from "../component/pingyinConverter";
 import ScrollList from "../component/scorllList/index";
 import ScrollTable from '../component/srollTable';
-const tableData = [
-    {
-        order: '1122',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '1221',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '1341',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '2734',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '2734',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '2734',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    },
-    {
-        order: '2734',
-        location: '浙江杭州',
-        today: '123',
-        sevenDays: '322',
-        recent: '1-20 14:30'
-    }
-];
-
 
 const rightPage = (props) => {
     const { location } = props;
     const [ globalDevice, setGlobalDevice ] = useState(0);
     const [ districtDevice, setDistrictDevice ] = useState(0);
     const [ onlineDevice, setOnlineDevice] = useState(0);
-    const [ reportData, setReportData ] = useState([]);
 
     useEffect(()=> {
         loadDeviceSummary();
     },[]);
     useEffect(() => {
         loadDistrictDevice(location);
-        loadReportList(location);
     },[location])
     const loadDeviceSummary = () => {
         loadGlobalDevice();
@@ -94,17 +40,6 @@ const rightPage = (props) => {
                 setOnlineDevice(res.total);
             })
     };
-    const loadReportList =  (location) => {
-        let lc = pinyinConverter(location);
-        if (lc !== 'zhongguo' && lc !== 'zhejiangsheng' && lc !== 'hangzhoushi') {
-            lc = 'zhongguo';
-        }
-        getReportListData(lc)
-            .then((res) => {
-                setReportData(res.splice(0,5));
-            })
-    };
-
 
     return (
         <div className="right-container">
@@ -139,18 +74,6 @@ const rightPage = (props) => {
                         <div className="table-title-recent">最近检测时间</div>
                     </div>
                     <ScrollTable />
-                    {/*<ul className="table-ul">*/}
-                    {/*    {tableData.map((item, index) => {*/}
-                    {/*        return (<li className="table-item" key={index}>*/}
-                    {/*                <div className="table-item-icon"></div>*/}
-                    {/*                <div className="table-item-order">{item.order}</div>*/}
-                    {/*                <div className="table-item-location">{item.location}</div>*/}
-                    {/*                <div className="table-item-today">{item.today}</div>*/}
-                    {/*                <div className="table-item-sevendays">{item.sevenDays}</div>*/}
-                    {/*                <div className="table-item-recent">{item.recent}</div>*/}
-                    {/*        </li>)*/}
-                    {/*    })}*/}
-                    {/*</ul>*/}
                 </div>
             </div>
         </div>
